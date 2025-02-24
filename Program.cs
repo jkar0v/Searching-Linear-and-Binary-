44512b192430d7e16549833f8bcf4f40bacfb996
MEGA <MEGA@gmail.com>
2025-02-24 17:05:21 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Searching__Linear_and_Binary_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+
+    }
+}
