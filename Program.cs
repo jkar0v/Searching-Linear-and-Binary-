edea0b1162df8d081928b08393ed47cc6c3a9962
MEGA <MEGA@gmail.com>
2025-02-24 17:28:14 +0200

    Added zad 1

--- Program.cs
+++ Program.cs
@@ -4,8 +4,40 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
-        }
+            int[] array =  GenerateArray(100);
+            foreach (var item in array)
+            {
+                Console.Write(item + ", ");
+            }
+            Console.WriteLine("\nEnter the number you want to search: ");
+            int search = int.Parse(Console.ReadLine());
+            (int count, List<int> positions) = LinearSearch(array, search);
+            Console.WriteLine($"Found {search} {count} times at {string.Join(',', positions)} positions.");
 
+        }
+        static (int, List<int>) LinearSearch(int[] array, int search)
+        {
+            int elementsCount = 0;
+            List<int> values = new List<int>();
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] == search)
+                {
+                    elementsCount++;
+                    values.Add(i);
+                }
+            }
+            return (elementsCount, values);
+        }
+        static int[] GenerateArray(int size)
+        {
+            Random random = new Random();
+            int[] array = new int[size];
+            for (int i = 0; i < size; i++)
+            {
+                array[i] = random.Next(1, 100);
+            }
+            return array;
+        }
     }
 }
