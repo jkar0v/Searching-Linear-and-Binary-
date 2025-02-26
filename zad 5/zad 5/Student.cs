da84d2fe52ba768bc4cbe2f0e8b009539c8f173d
MEGA <MEGA@gmail.com>
2025-02-26 17:30:30 +0200

    zad 5

---
+++ zad 5/zad 5/Student.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zad_5
+{
+    internal class Student
+    {
+        public string Name { get; set; }
+        double Score { get; set; }
+
+    }
+}
---
+++ zad 5/zad 5/University.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zad_5
+{
+    internal class University
+    {
+        List<Student> students;
+
+        public University()
+        {
+            students = new List<Student>();
+        }
+        public void AddStudent(Student student)
+        {
+            students.Add(student);
+            SortStudents();
+        }
+        private void SortStudents()
+        {
+            for (int i = 0; i < students.Count - 1; i++)
+            {
+                for (int j = 0; j < students.Count - i - 1; j++)
+                {
+                    if (students[j].Name.CompareTo(students[j + 1].Name) == -1)
+                        (students[j], students[j + 1]) = (students[j + 1], students[j]);
+                }
+            }
+        }
+    }
+}
