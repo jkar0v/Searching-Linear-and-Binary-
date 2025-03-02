7a9a00f62fec33cb315d383c6e8ee5245e65b09a
Jivko Karov <jikito2007@gmail.com>
2025-03-02 19:15:30 +0200

    Added zad 6 fully finished

---
+++ zad 6/Company.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zad_6
+{
+    class Company
+    {
+        List<Employee> employees;
+        public Company()
+        {
+            employees = new List<Employee>();
+        }
+        internal void AddEmployee(Employee employee)
+        {
+            employees.Add(employee);
+        }
+        internal void SearchEmployee(string name)
+        {
+            foreach (Employee employee in employees)
+            {
+                if (employee.Name == name)
+                {
+                    Console.WriteLine("Employee found: " + employee.Name + " " + employee.Salary);
+                    return;
+                }
+            }
+            Console.WriteLine("Employee not found");
+        }
+    }
+}
--- zad 6/Program.cs
+++ zad 6/Program.cs
@@ -4,7 +4,15 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Company company = new Company();
+            company.AddEmployee(new Employee("John", 5000));
+            company.AddEmployee(new Employee("Bill", 3000));
+            company.AddEmployee(new Employee("Steve", 2000));
+            company.AddEmployee(new Employee("Elon", 10000));
+            company.AddEmployee(new Employee("Jeff", 15000));
+
+            company.SearchEmployee("Elon");
+            company.SearchEmployee("Bill");
         }
     }
 }
